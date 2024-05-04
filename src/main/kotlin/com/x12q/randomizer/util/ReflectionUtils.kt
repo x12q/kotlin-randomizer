b79f693f02cfece41900e09ce78c1f2572e640e4
phong <31875822+phongqwe@users.noreply.github.com>
2024-05-04 13:04:34 +0200

    update

--- src/main/kotlin/com/x12q/randomizer/util/ReflectionUtils.kt
+++ src/main/kotlin/com/x12q/randomizer/util/ReflectionUtils.kt
@@ -11,7 +11,6 @@ import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.starProjectedType
 
 object ReflectionUtils {
-
     private fun createRandomizer(clazz: KClass<out Randomizer<*>>): Randomizer<*> {
         val obj = clazz.objectInstance
         if (obj != null) {
@@ -30,69 +29,4 @@ object ReflectionUtils {
     fun createParamRandomizer(clazz: KClass<out ParameterRandomizer<*>>): ParameterRandomizer<*> {
         return createRandomizer(clazz) as ParameterRandomizer<*>
     }
-
-    /**
-     * Check if a [KType] contains any generic type that match [targetClass] or is children of [targetClass]
-     */
-    @Deprecated("dont use")
-    fun KType.canProduceGeneric(targetClass: KClass<*>): Boolean {
-        return this.arguments.map {
-            // take variance into consideration
-            // it.variance
-            val variance = it.variance
-            when (variance) {
-                KVariance.INVARIANT, KVariance.OUT -> {
-                    it.type?.classifier
-                }
-
-                else -> null
-            }
-        }.any { classifier ->
-            (classifier as? KClass<*>)?.let {
-                targetClass == it || targetClass.isSubclassOf(it)
-            } ?: false
-        }
-    }
-
-    /**
-     * Check if a [KClass] can be assigned to any generic types within [KType].
-     * Eg:
-     * check if A is assignable the generic of List<A>
-     */
-    @Deprecated("dont use")
-    fun KClass<*>.isAssignableToGenericOf(kType: KType): Boolean {
-        val parentTypes = this.supertypes + this.starProjectedType
-        return kType.arguments.any { arg ->
-            parentTypes.contains(arg.type)
-        }
-    }
-
-    /**
-     * This function is more completed. It can check for generic assignability even at children class.
-     * Such as: class ABC: List<Int>
-     *     Int.isAssignableToGenericOf(ABC) -> is true
-     *     The problem with this one is that, it is very slow.
-     *     only need to check assignable to a specific interface, a better way is to check it directly against that interface.
-     *     -> no need to lookup for parent class or anything.
-     */
-    @Deprecated("dont use")
-    fun KClass<*>.isAssignableToGenericOf(kClass: KClass<*>): Boolean {
-        val allTypes = this.supertypes + this.starProjectedType
-        val allArg = (kClass.supertypes.flatMap { it.arguments } + kClass.starProjectedType.arguments).map { it.type }
-        return allTypes.any {
-            it in allArg
-        }
-    }
-
-    /**
-     * check if a [KClass] can be assigned to a particular [KType]
-     * Eg:
-     * if class A is assignable to class B
-     */
-    @Deprecated("dont use")
-    fun KClass<*>.isAssignableTo(kType: KType): Boolean {
-        val parentTypes = supertypes + starProjectedType
-        return parentTypes.contains(kType)
-    }
-
 }
--- src/test/kotlin/com/x12q/randomizer/util/ReflectionUtilsTest.kt
+++ src/test/kotlin/com/x12q/randomizer/util/ReflectionUtilsTest.kt
@@ -5,16 +5,10 @@ import com.x12q.randomizer.randomizer.ClassRandomizer
 import com.x12q.randomizer.randomizer.ParamInfo
 import com.x12q.randomizer.randomizer.ParameterRandomizer
 import com.x12q.randomizer.randomizer.RDClassData
-import com.x12q.randomizer.util.ReflectionUtils.canProduceGeneric
-import com.x12q.randomizer.util.ReflectionUtils.isAssignableTo
-import com.x12q.randomizer.util.ReflectionUtils.isAssignableToGenericOf
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.matchers.booleans.shouldBeFalse
-import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import kotlin.reflect.KParameter
-import kotlin.reflect.typeOf
 import kotlin.test.Test
 
 class ReflectionUtilsTest {
@@ -98,72 +92,5 @@ class ReflectionUtilsTest {
         shouldNotThrow<Throwable> {
             ReflectionUtils.createParamRandomizer(PR2::class) shouldBe PR2
         }
-    }
-
-    open class A
-    interface B
-    open class A1 : B, A()
-    class A2 : A1()
-    class B1 : B
-
-
-    @Test
-    fun containGeneric() {
-        typeOf<List<A>>().canProduceGeneric(A1::class).shouldBeTrue()
-        typeOf<List<A>>().canProduceGeneric(A2::class).shouldBeTrue()
-
-        typeOf<List<B>>().canProduceGeneric(A1::class).shouldBeTrue()
-        typeOf<List<B>>().canProduceGeneric(A2::class).shouldBeTrue()
-
-        typeOf<List<A1>>().canProduceGeneric(A2::class).shouldBeTrue()
-
-        typeOf<List<A>>().canProduceGeneric(B::class).shouldBeFalse()
-
-
-    }
-
-    @Test
-    fun checkAssignable() {
-        A::class.isAssignableTo(typeOf<A>()) shouldBe true
-        A1::class.isAssignableTo(typeOf<A>()) shouldBe true
-
-        B::class.isAssignableTo(typeOf<B>()) shouldBe true
-        B1::class.isAssignableTo(typeOf<B>()) shouldBe true
-        A1::class.isAssignableTo(typeOf<B>()) shouldBe true
-
-        A::class.isAssignableTo(typeOf<A1>()) shouldBe false
-        B::class.isAssignableTo(typeOf<B1>()) shouldBe false
-
-        B::class.isAssignableTo(typeOf<A>()) shouldBe false
-        B::class.isAssignableTo(typeOf<A1>()) shouldBe false
-
-        A::class.isAssignableTo(typeOf<B>()) shouldBe false
-        A1::class.isAssignableTo(typeOf<B1>()) shouldBe false
-    }
-
-    @Test
-    fun checkGenericAssignable() {
-
-        A::class.isAssignableToGenericOf(typeOf<List<A>>()) shouldBe true
-        B::class.isAssignableToGenericOf(typeOf<List<B>>()) shouldBe true
-        A1::class.isAssignableToGenericOf(typeOf<List<A1>>()) shouldBe true
-        B1::class.isAssignableToGenericOf(typeOf<List<B1>>()) shouldBe true
-
-        A1::class.isAssignableToGenericOf(typeOf<List<A>>()) shouldBe true
-        B::class.isAssignableToGenericOf(typeOf<List<A>>()) shouldBe false
-        B1::class.isAssignableToGenericOf(typeOf<List<A>>()) shouldBe false
-
-        A::class.isAssignableToGenericOf(typeOf<List<B>>()) shouldBe false
-        A1::class.isAssignableToGenericOf(typeOf<List<B>>()) shouldBe true
-        B1::class.isAssignableToGenericOf(typeOf<List<B>>()) shouldBe true
-
-        A::class.isAssignableToGenericOf(typeOf<List<A1>>()) shouldBe false
-        B::class.isAssignableToGenericOf(typeOf<List<A1>>()) shouldBe false
-        B1::class.isAssignableToGenericOf(typeOf<List<A1>>()) shouldBe false
-
-        B::class.isAssignableToGenericOf(typeOf<List<B1>>()) shouldBe false
-        A::class.isAssignableToGenericOf(typeOf<List<B1>>()) shouldBe false
-        A1::class.isAssignableToGenericOf(typeOf<List<B1>>()) shouldBe false
-
     }
 }
