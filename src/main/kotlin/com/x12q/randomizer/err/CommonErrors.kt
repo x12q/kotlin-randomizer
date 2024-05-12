5d19caf087e2ec30c42b94d00c4328ae9b089783
phong <31875822+phongqwe@users.noreply.github.com>
2024-05-12 17:09:13 +0200

    remove unused code

--- src/main/kotlin/com/x12q/randomizer/err/CommonErrors.kt
+++ src/main/kotlin/com/x12q/randomizer/err/CommonErrors.kt
@@ -7,30 +7,6 @@ object CommonErrors {
     private fun errCode(code:Int):String{
         return "$prefix${code}"
     }
-    private fun makeCommonExceptionErrorReport(templateHeader: ErrorHeader,detail:String?,exception: Throwable): ErrorReport {
-        return SingleErrorReport(
-            header = templateHeader.let { errHeader ->
-                detail?.let {
-                    errHeader.setDescription(detail)
-                } ?: templateHeader
-            },
-        )
-    }
-
-
-    /**
-     * this error indicates that an exception was caught
-     */
-    object ExceptionError {
-        val header = ErrorHeader(errCode(3), "Exception error")
-
-        fun report(exception: Throwable): ErrorReport {
-            return makeCommonExceptionErrorReport(header.appendDescription(":${exception}"),null,exception)
-        }
-        fun report(message:String?,exception: Throwable): ErrorReport {
-            return makeCommonExceptionErrorReport(header,message,exception)
-        }
-    }
 
     object MultipleErrors {
         val header = ErrorHeader(errCode(4), "Multiple errors")
