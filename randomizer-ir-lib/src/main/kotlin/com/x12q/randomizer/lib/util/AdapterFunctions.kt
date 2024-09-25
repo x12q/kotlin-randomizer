d7e5025b46194cd38cb0850d27439adbec49f51a
phong-shape <phong@shape.dk>
2024-09-25 09:44:36 +0200

    refactor

--- randomizer-ir-lib/src/main/kotlin/com/x12q/randomizer/lib/util/AdapterFunctions.kt
+++ randomizer-ir-lib/src/main/kotlin/com/x12q/randomizer/lib/util/AdapterFunctions.kt
@@ -1,9 +1,11 @@
 package com.x12q.randomizer.lib.util
 
+import com.x12q.randomizer.lib.ForGeneratedCodeOnly
+
 /**
  * Guide:
  * - This file contains adapter functions that make it safer & more reliable for the randomizer plugin to lookup different overloads of functions from the kotlin standard library.
- * - These functions are not meant to be used by anything other than the randomizer plugin.
+ * - However, these functions should not be called by anything other than the generated code
  * - Functions in this file must not have more than 1 overload.
  * - If there's a need for overloading, use a unique new name instead. That way, the plugin can avoid perform filter by itself.
  */
@@ -12,6 +14,7 @@ package com.x12q.randomizer.lib.util
 /**
  * Adapter function for making List. This is not meant to be used by anything other than the randomizer plugin.
  */
+@ForGeneratedCodeOnly
 fun <T> makeList(size:Int, makeElement:(index:Int)->T):List<T>{
     return List(size,makeElement)
 }
@@ -19,6 +22,7 @@ fun <T> makeList(size:Int, makeElement:(index:Int)->T):List<T>{
 /**
  * Adapter function for making Map. This is not meant to be used by anything other than the randomizer plugin.
  */
+@ForGeneratedCodeOnly
 fun <K, V> makeMap(
     size: Int,
     makeKey: () -> K,
@@ -34,6 +38,7 @@ fun <K, V> makeMap(
 /**
  * Adapter function for making Pair. This is not meant to be used by anything other than the randomizer plugin.
  */
+@ForGeneratedCodeOnly
 fun <K, V> makePair(key: K, value: V): Pair<K, V> {
     return key to value
 }
@@ -41,6 +46,7 @@ fun <K, V> makePair(key: K, value: V): Pair<K, V> {
 /**
  * Adapter function to convert a list to a set. This is not meant to be used by anything other than the randomizer plugin.
  */
+@ForGeneratedCodeOnly
 fun <T> listToSet(list:List<T>):Set<T>{
     return list.toSet()
 }
