2364a8825d27ebdc21117c35d7b9c8acfb7a9faf
x12q <31875822+x12q@users.noreply.github.com>
2024-12-19 14:17:02 +0100

    remove front end (#32)

--- sample-app/src/main/kotlin/com/x12q/sample_app/Main.kt
+++ sample-app/src/main/kotlin/com/x12q/sample_app/Main.kt
@@ -5,30 +5,32 @@ import com.x12q.randomizer.lib.RandomContextBuilderFunctions.constant
 import com.x12q.randomizer.lib.RandomContextBuilderFunctions.factory
 import com.x12q.randomizer.lib.annotations.Randomizable
 import java.util.Date
+import com.x12q.randomizer.lib.random
 
-@Randomizable
 data class ABC<T, E>(val qxParam: T, val map: Map<Double, String>, val l: List<E>, val q: Date)
-
+data class Q(val i:ABC<String,Int>)
 fun main() {
+    // println(random<ABC<Int,Long>>())
+    println(random<Q>())
     // println(Qx.random<Int,Long>(randomizers={
     //     constant{666}
     //     factory{ listOf("a","b").random() }
     // }))
 
-    val abc1 = ABC.random<Int, Long>(
-        randomizers = {
-            val randomContextBuilder = this
-            ////
-        }
-    )
-
-    val abc2 = ABC.random<Int,Long>(
-        randomConfig = RandomConfigImp.default,
-        randomizers= {
-            val randomContextBuilder = this
-            ////
-        }
-    )
+    // val abc1 = random<ABC<Int, Long>>(
+    //     randomizers = {
+    //         val randomContextBuilder = this
+    //         ////
+    //     }
+    // )
+    //
+    // val abc2 = ABC.random<Int,Long>(
+    //     randomConfig = RandomConfigImp.default,
+    //     randomizers= {
+    //         val randomContextBuilder = this
+    //         ////
+    //     }
+    // )
 
 
 }
