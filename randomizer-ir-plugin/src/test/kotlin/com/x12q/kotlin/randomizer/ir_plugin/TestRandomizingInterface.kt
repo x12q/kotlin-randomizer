43ec5c2c8bec235925914e72103041d8babd36af
phong <31875822+phongqwe@users.noreply.github.com>
2024-12-22 15:14:20 +0100

    add code to choose candidate among classes passed to @Randomizable

--- randomizer-ir-plugin/src/test/kotlin/com/x12q/kotlin/randomizer/ir_plugin/TestRandomizingInterface.kt
+++ randomizer-ir-plugin/src/test/kotlin/com/x12q/kotlin/randomizer/ir_plugin/TestRandomizingInterface.kt
@@ -9,6 +9,7 @@ import com.x12q.kotlin.randomizer.test.util.test_code.TestImportsBuilder
 import io.kotest.matchers.shouldBe
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import kotlin.test.Test
+import kotlin.test.fail
 
 
 @OptIn(ExperimentalCompilerApi::class)
@@ -29,20 +30,12 @@ class TestRandomizingInterface {
         override val str: String = "zzz"
     }
 
-    interface GenericInterface<T1,T2>{
-        val t1:T1
-        val t2:T2
-    }
-
-    class GenericImplementation_1<G0>(override val t1: Int, override val t2: G0) : GenericInterface<Int,G0>{
-
-    }
-
     private val imports = TestImportsBuilder.stdImport
         .import(QxC::class)
         .import(PlainInterface::class)
         .import(GenericInterface::class)
         .import(TestRandomConfigForAbstractClassAndInterface::class)
+        .import(GenericImplementation_1::class)
 
     data class QxC<K_Q:Any>(override val data:K_Q):WithData
 
@@ -75,6 +68,45 @@ class TestRandomizingInterface {
                         d = t.nextDouble()
                     )
                 )
+            }
+        }
+    }
+
+    @Randomizable(
+        candidates = [GenericImplementation_1::class, GenericImplementation_2::class],
+    )
+    interface GenericInterface<T1,T2>{
+        val t1:T1
+        val t2:T2
+    }
+
+    class GenericImplementation_1<T_0>(override val t1: Int, override val t2: T_0) : GenericInterface<Int,T_0>
+    class GenericImplementation_2<E_0,E_1>(override val t1: E_1, override val t2: E_0) : GenericInterface<E_1,E_0>
+
+    // => this one should throw an error
+    class GenericImplementation_3<H_0,H_1, H_3>(override val t1: H_1, override val t2: H_0, val t3: H_3) : GenericInterface<H_1,H_0>
+    /**
+     * Test passing generic param from "random" function to generic with a property.
+     * The generic type is further passed down to deeper layer within the class of that property.
+     */
+    @Test
+    fun `generate random generic interface`() {
+        testGeneratedCodeUsingStandardPlugin(
+            """
+                $imports
+
+                fun runTest():TestOutput {
+                    return withTestOutput {
+                        putData(random<QxC<PlainInterface>>(randomConfig=TestRandomConfigForAbstractClassAndInterface(0)))
+                        putData(random<QxC<PlainInterface>>(randomConfig=TestRandomConfigForAbstractClassAndInterface(1)))
+                    }
+                }
+            """,
+        ) {
+            testCompilation = { result, _ ->
+                result.exitCode shouldBe KotlinCompilation.ExitCode.OK
+                val objectList = result.executeRunTestFunction().getObjs()
+                fail("CONTINUE here")
             }
         }
     }
