70b3478e1e84398198bd396062027e666f6a594d
phong <31875822+phongqwe@users.noreply.github.com>
2024-12-27 14:16:07 +0100

    temp commit

--- sample-app/src/main/kotlin/com/x12q/sample_app/Main.kt
+++ sample-app/src/main/kotlin/com/x12q/sample_app/Main.kt
@@ -10,7 +10,7 @@ import com.x12q.kotlin.randomizer.lib.random
 
 data class ABC<T, E>(val qxParam: T, val map: Map<Double, String>, val l: List<E>, val q: Date)
 data class XYZ<T, E>(val t: T, val e:E)
-// data class Q(val i:ABC<String,Int>)
+data class Q(val i:ABC<String,Int>)
 data class QxC<Z>(val z:Z)
 
 fun main() {
@@ -20,17 +20,9 @@ fun main() {
         long(456L)
     }))
     println(random<QxC<XYZ<Int,Long>>>(randomizers = {
-        // constant<Int>(123)
         int(123)
         long{
             random<Int>().toLong()
         }
-        // double{
-        //     listOf(1.0,2.0,3.0).random()
-        // }
-        // string{
-        //     random<Int>().toString()
-        // }
     }))
-    // println(random<Q>())
 }
