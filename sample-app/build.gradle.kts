a0aaae182ecf50150ec0a916fe31bfd3b0e6e99e
phong <31875822+phongqwe@users.noreply.github.com>
2025-01-16 00:04:16 +0100

    update sample-app

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -3,9 +3,9 @@ import org.gradle.api.logging.LogLevel
 import org.gradle.kotlin.dsl.invoke
 
 plugins {
-    kotlin("jvm") version "2.1.0"
-    id("com.x12q.kotlin.randomizer") version "1.0.0-alpha.13-2.1.0"
-    kotlin("plugin.serialization") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
+    id("com.x12q.kotlin.randomizer") version "1.0.0-alpha.14-2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 
@@ -14,16 +14,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
+    // mavenLocal()
 }
 kotlin {
     val javaVersion = 8
     jvmToolchain(javaVersion)
 }
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
     testImplementation(kotlin("test"))
-    implementation("com.x12q:kotlin-randomizer-lib:1.0.0-alpha.13-2.1.0")
+    implementation("com.x12q:kotlin-randomizer-lib:1.0.0-alpha.14-2.0.20")
 }
 
 kotlinRandomizer{
--- sample-app/src/main/kotlin/com/x12q/sample_app/Main.kt
+++ sample-app/src/main/kotlin/com/x12q/sample_app/Main.kt
@@ -1,5 +1,6 @@
 package com.x12q.sample_app
 
+import com.x12q.kotlin.randomizer.lib.RandomConfig
 import com.x12q.kotlin.randomizer.lib.RandomContextBuilderFunctions.constant
 import com.x12q.kotlin.randomizer.lib.RandomContextBuilderFunctions.double
 import com.x12q.kotlin.randomizer.lib.RandomContextBuilderFunctions.int
@@ -7,14 +8,21 @@ import com.x12q.kotlin.randomizer.lib.RandomContextBuilderFunctions.long
 import com.x12q.kotlin.randomizer.lib.RandomContextBuilderFunctions.string
 import java.util.Date
 import com.x12q.kotlin.randomizer.lib.random
+import kotlinx.serialization.Serializable
+import kotlin.random.Random
 
 data class ABC<T, E>(val qxParam: T, val map: Map<Double, String>, val l: List<E>, val q: Date)
 data class XYZ<T, E>(val t: T, val e:E)
 data class Q(val i:ABC<String,Int>)
 data class QxC<Z>(val z:Z)
 
+@Serializable
+data class SE(val i:Int)
+
 
 fun main() {
+    val rdConfig = RandomConfig.defaultWith(random = Random(123))
+    println(random<SE>(randomConfig = rdConfig))
     println(random<Int>(randomizers = {int(123)}))
     println(random<XYZ<Int,Long>>(randomizers = {
         int(123)
